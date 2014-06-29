db40a059bfe52556fb2b32ceec61f2b09762c492
scarerkite <scknight101@gmail.com>
2014-06-29 14:40:26 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Meetapp::Application.config.secret_key_base = 'b468a8f535cc527138bcff1a83a03f270cd11c2aee8926d76a142a9fb24a9a928e7b774c9658475dc64bf2b9bc3c7a431de263bbf69794c3f610ed25c685a2e9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Meetapp::Application.config.session_store :cookie_store, key: '_meetapp_session'
