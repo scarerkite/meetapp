bd2b9b3b71397a062d88dbb7d283c41f53a9e203
scarerkite <scknight101@gmail.com>
2014-07-01 10:50:17 +0100

    Change events index to only show users' events

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ActiveRecord::Base
 
   has_many :events
   has_many :comments
-  has_many :events, through: :invitations
+  has_many :invitations
+  # has_many :events, through: :invitations
 end
