d5f20790ee3979930163e3bd48da608cf623faad
scarerkite <scknight101@gmail.com>
2014-07-07 10:21:11 +0100

    Added helpers, and buttons

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,8 +1,8 @@
 class FriendshipsController < Devise::RegistrationsController
-  before_action :set_friendship, only: [:show, :edit, :update, :destroy]
 
   def create
-    @friendship = current_user.friendships.build(friend_id: params[:friend_id])
+    @friendship.user = current_user
+    @friendship.friend = User.find(params[:id])
 
     if @friendship.save
       redirect_to root_url, notice: 'Friend was successfully added.'
--- app/helpers/invitations_helper.rb
+++ app/helpers/invitations_helper.rb
@@ -1,2 +1,14 @@
 module InvitationsHelper
+
+  def accept_invite(invitation)
+    invitation.accepted = true
+    invitation.save
+  end
+
+  def decline_invite(invitation)
+    invitation.accepted = false
+    invitation.save
+  end
+
+
 end
