54268faa8e9013fcac07aa8b3f03070b297885c3
scarerkite <scknight101@gmail.com>
2014-07-09 18:20:25 +0100

    Sorted some styling issues. Manually added coords to seed data.

--- db/seeds/event_data.rb
+++ db/seeds/event_data.rb
@@ -9,17 +9,22 @@ Event.create!(
   postcode: "EC1R 0EG",
   date: "21-08-2014",
   time: "2000-01-01 21:00:00 UTC",
-  host_id: 1
+  host_id: 1,
+  latitude: 51.5232500,
+  longitude: -0.1052060
+
 )
 
 Event.create!(
   title: "Get together",
   description: "Knees up",
-  address: "90 Walton St, Oxford",
+  address: "The Victoria, 90 Walton St, Oxford",
   postcode: "OX2 6EB",
   date: "14-10-2014",
   time: "2000-01-01 20:00:00 UTC",
-  host_id: 5
+  host_id: 5,
+  latitude: 51.7612010,
+  longitude: -1.2674940
 )
 
 Event.create!(
@@ -29,185 +34,91 @@ Event.create!(
   postcode: "EC1R 5DU",
   date: "21-07-2014",
   time: "2000-01-01 19:00:00 UTC",
-  host_id: 5
+  host_id: 5,
+  latitude: 51.5221910,
+  longitude: -0.1109010
 )
 
 Event.create!(
   title: "Charlotte's birthday",
   description: "Charlotte's birthday do.",
-  address: "The Fishes, North Hinksey Village, Oxford",
-  postcode: "OX2 0NA",
+  address: "Level 31, The Shard, 31 St Thomas St, London",
+  postcode: "SE1 9RY",
   date: "24-07-2014",
   time: "2000-01-01 19:00:00 UTC",
-  host_id: 4
+  host_id: 4,
+  latitude: 51.5044490,
+  longitude: -0.0866010
 )
 
 Event.create!(
   title: "GA drinks",
-  description: "Shindig",
-  address: "9 Back Hill",
-  postcode: "EC1R 5EN",
+  description: "Get together",
+  address: "The Coach & Horses, 26-28 Ray St, Clerkenwell, London",
+  postcode: "EC1R 3DJ",
   date: "15-07-2014",
   time: "2000-01-01 18:00:00 UTC",
-  host_id: 1
+  host_id: 1,
+  latitude: 51.5230360,
+  longitude: -0.1092870
 )
 
 Event.create!(
   title: "Charlotte's leaving do",
   description: "Charlotte's farewell",
-  address: "13 Eyre Street Hill",
+  address: "The Gunmakers, 13 Eyre Street Hill",
   postcode: "EC1R 5ET",
   date: "24-08-2014",
   time: "2000-01-01 19:00:00 UTC",
-  host_id: 7
+  host_id: 7,
+  latitude: 51.5225550,
+  longitude: -0.1104130
 )
 
 Event.create!(
-  title: "Good riddance Hilda",
+  title: "Good riddance Hilda!",
   description: "Hilda's going away party",
   address: "The Crown Tavern, 43 Clerkenwell Green",
   postcode: "EC1R 0EG",
   date: "21-08-2014",
   time: "2000-01-01 19:00:00 UTC",
-  host_id: 1
+  host_id: 1,
+  latitude: 51.5232500,
+  longitude: -0.1052060
 )
 
 Event.create!(
-  title: "Get together",
-  description: "Knees up",
-  address: "90 Walton St, Oxford",
-  postcode: "OX2 6EB",
+  title: "July get together",
+  description: "Fun times",
+  address: "The Isis Tavern, Oxford",
+  postcode: "OX4 4EL",
   date: "14-10-2014",
   time: "2000-01-01 18:30:00 UTC",
-  host_id: 5
-)
-
-Event.create!(
-  title: "Let's meet up",
-  description: "Shindig",
-  address: "The Clerk And Well, 156 Clerkenwell Rd, London",
-  postcode: "EC1R 5DU",
-  date: "21-07-2014",
-  time: "2000-01-01 19:00:00 UTC",
-  host_id: 2
+  host_id: 5,
+  latitude: 51.7315100,
+  longitude: -1.2416050
 )
 
 Event.create!(
   title: "Ida's birthday",
   description: "Ida's birthday do.",
-  address: "The Fishes, North Hinksey Village, Oxford",
-  postcode: "OX2 0NA",
+  address: "The Rusty Bicycle, 28 Magdalen Rd, Oxford",
+  postcode: "OX4 1RB",
   date: "24-07-2014",
   time: "2000-01-01 18:00:00 UTC",
-  host_id: 3
+  host_id: 3,
+  latitude: 51.7423310,
+  longitude: -1.2353700
 )
 
 Event.create!(
-  title: "GA drinks",
-  description: "Shindig",
-  address: "9 Back Hill",
-  postcode: "EC1R 5EN",
-  date: "15-07-2014",
+  title: "August Meet Up",
+  description: "Catch up.",
+  address: "Atomic Pizza, 247 Cowley Rd, Oxford",
+  postcode: "OX4 1XG",
+  date: "15-08-2014",
   time: "2000-01-01 18:00:00 UTC",
-  host_id: 1
-)
-
-Event.create!(
-  title: "Bye Bye Jack",
-  description: "Jack's going away bash",
-  address: "The Crown Tavern, 43 Clerkenwell Green",
-  postcode: "EC1R 0EG",
-  date: "21-08-2014",
-  time: "2000-01-01 18:00:00 UTC",
-  host_id: 1
-)
-
-Event.create!(
-  title: "Get together",
-  description: "Knees up",
-  address: "90 Walton St, Oxford",
-  postcode: "OX2 6EB",
-  date: "14-10-2014",
-  time: "2000-01-01 19:00:00 UTC",
-  host_id: 5
-)
-
-Event.create!(
-  title: "Let's meet up",
-  description: "Shindig",
-  address: "The Clerk And Well, 156 Clerkenwell Rd, London",
-  postcode: "EC1R 5DU",
-  date: "21-07-2014",
-  time: "2000-01-01 19:00:00 UTC",
-  host_id: 5
-)
-
-Event.create!(
-  title: "Charlotte's birthday",
-  description: "Charlotte's birthday do.",
-  address: "The Fishes, North Hinksey Village, Oxford",
-  postcode: "OX2 0NA",
-  date: "24-07-2014",
-  time: "2000-01-01 19:00:00 UTC",
-  host_id: 4
-)
-
-Event.create!(
-  title: "GA drinks",
-  description: "Shindig",
-  address: "9 Back Hill",
-  postcode: "EC1R 5EN",
-  date: "15-07-2014",
-  time: "2000-01-01 19:00:00 UTC",
-  host_id: 1
-)
-
-Event.create!(
-  title: "Bye Bye Hilda",
-  description: "Hilda's going away party",
-  address: "The Crown Tavern, 43 Clerkenwell Green",
-  postcode: "EC1R 0EG",
-  date: "21-08-2014",
-  time: "2000-01-01 19:00:00 UTC",
-  host_id: 1
-)
-
-Event.create!(
-  title: "Get together",
-  description: "Knees up",
-  address: "90 Walton St, Oxford",
-  postcode: "OX2 6EB",
-  date: "14-10-2014",
-  time: "2000-01-01 19:00:00 UTC",
-  host_id: 5
-)
-
-Event.create!(
-  title: "Let's meet up",
-  description: "Shindig",
-  address: "The Clerk And Well, 156 Clerkenwell Rd, London",
-  postcode: "EC1R 5DU",
-  date: "21-07-2014",
-  time: "2000-01-01 19:00:00 UTC",
-  host_id: 6
-)
-
-Event.create!(
-  title: "Ida's birthday",
-  description: "Ida's birthday do.",
-  address: "The Fishes, North Hinksey Village, Oxford",
-  postcode: "OX2 0NA",
-  date: "24-07-2014",
-  time: "2000-01-01 20:00:00 UTC",
-  host_id: 7
-)
-
-Event.create!(
-  title: "GA drinks",
-  description: "Shindig",
-  address: "9 Back Hill",
-  postcode: "EC1R 5EN",
-  date: "15-07-2014",
-  time: "2000-01-01 19:00:00 UTC",
-  host_id: 8
+  host_id: 1,
+  latitude: 51.7457080,
+  longitude: -1.2307650
 )
